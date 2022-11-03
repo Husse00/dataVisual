f0b36f559c9f6be0662728dd098d5a7303ea6389
Husse00 <fhusal19@gmail.com>
2022-11-03 12:34:16 +0100

    update

--- datavisual.js
+++ datavisual.js
@@ -1,14 +1,26 @@
 let root = document.getElementById("root")
-
+let data = document.createElement("div")
+data.classList.add("data")
 fetch('data.json').then(function (response){
 return response.json();
 
 }).then(function(obj){
-    
-    console.log(obj)
+   
+    console.log(obj.attribut)
+    data.innerHTML +=
+    `Tilltalsnamn: ${obj.attribut.tilltalsnamn} --
+    Efternamn: ${obj.attribut.efternamn} --
+    Födelsedatum: ${obj.attribut.födelsedatum} --
+    folkbokförinsadress: ${obj.attribut.folkbokförinsAdress} --
+    särskild postadress: ${obj.attribut.särskildpostAdress} --
+    E-postadress Arbetet: ${obj.attribut.EpostadressaArbetet} --
+    Län: ${obj.attribut.län}`
+
+
+    root.append(data)
     
   
-}).catch(function(error){
+}).catch(function(err){
 
-    console.log(error + "404")
+    console.log(err + "404")
 })
