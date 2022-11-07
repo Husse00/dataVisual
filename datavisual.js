534325bacac0d686684111ecd2777be0cecbc94a
Al-Yasiri <SE2321000081-hu2016@ltblekinge.se>
2022-11-07 10:49:11 +0100

    upd

--- datavisual.js
+++ datavisual.js
@@ -1,27 +1,50 @@
 let root = document.getElementById("root")
-let data = document.createElement("div")
-data.classList.add("data")
+let data = document.getElementById("data")
+let divAttribute = document.getElementById("divAttribute")
+// CREATING PARAGRAPHS TO STORE THE FECTHED DATA IN.
+
+let tilltalsnamn = document.createElement("p"); tilltalsnamn.classList.add("data")
+let efternamn = document.createElement("p"); efternamn.classList.add("data")
+let fdatum = document.createElement("p"); fdatum.classList.add("data")
+
+
+
+
+// FETCHING THE DATA
 fetch('data.json').then(function (response){
 return response.json();
 
 }).then(function(obj){
    
     console.log(obj.attribut)
-    data.innerHTML += 
-   `Tilltalsnamn: ${obj.attribut.tilltalsnamn} --
-    Efternamn: ${obj.attribut.efternamn} --
+
+tilltalsnamn.innerHTML +=  `Tilltalsnamn: ${obj.attribut.tilltalsnamn}` ;
+efternamn.innerHTML += `Efternamn: ${obj.attribut.efternamn}`;
+fdatum.innerHTML += `Födelsedatum: ${obj.attribut.födelsedatum}`;
+
+
+` Efternamn: ${obj.attribut.efternamn}`,
+` 
     Födelsedatum: ${obj.attribut.födelsedatum} --
     folkbokförinsadress: ${obj.attribut.folkbokförinsAdress} --
     särskild postadress: ${obj.attribut.särskildpostAdress} --
     E-postadress Arbetet: ${obj.attribut.EpostadressaArbetet} --
    `;
 
-   
+
   
-    root.append(data)
+    data.append(tilltalsnamn,efternamn,fdatum)
     
   
 }).catch(function(err){
 
     console.log(err + "404")
+})
+data.style.display = "none"
+divAttribute.addEventListener('click', ()=>{
+    if(data.style.display == 'none'){
+        data.style.display = 'block';
+    } else {
+        data.style.display = 'none'
+    }
 })
