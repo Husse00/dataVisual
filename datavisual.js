7393e170402f8618d2f5129843ba3ae63fb0d14e
Al-Yasiri <SE2321000081-hu2016@ltblekinge.se>
2022-11-07 13:51:37 +0100

    upd

--- datavisual.js
+++ datavisual.js
@@ -6,10 +6,13 @@ let divAttribute = document.getElementById("divAttribute")
 let tilltalsnamn = document.createElement("p"); tilltalsnamn.classList.add("data")
 let efternamn = document.createElement("p"); efternamn.classList.add("data")
 let fdatum = document.createElement("p"); fdatum.classList.add("data")
-
-
-
-
+let folkbokförinsAdress = document.createElement("p"); folkbokförinsAdress.classList.add("data")
+let särskildpostAdress = document.createElement("p"); särskildpostAdress.classList.add("data")
+let EpostadressaArbetet = document.createElement("p"); EpostadressaArbetet.classList.add("data")
+let län = document.createElement("p"); län.classList.add("data")
+let kommun = document.createElement("P"); kommun.classList.add("data");
+let kod = document.createElement("p"); kod.classList.add("data");
+let TypAvMedarbetare = document.createElement("p"); TypAvMedarbetare.classList.add("data")
 // FETCHING THE DATA
 fetch('data.json').then(function (response){
 return response.json();
@@ -21,19 +24,15 @@ return response.json();
 tilltalsnamn.innerHTML +=  `Tilltalsnamn: ${obj.attribut.tilltalsnamn}` ;
 efternamn.innerHTML += `Efternamn: ${obj.attribut.efternamn}`;
 fdatum.innerHTML += `Födelsedatum: ${obj.attribut.födelsedatum}`;
-
-
-` Efternamn: ${obj.attribut.efternamn}`,
-` 
-    Födelsedatum: ${obj.attribut.födelsedatum} --
-    folkbokförinsadress: ${obj.attribut.folkbokförinsAdress} --
-    särskild postadress: ${obj.attribut.särskildpostAdress} --
-    E-postadress Arbetet: ${obj.attribut.EpostadressaArbetet} --
-   `;
-
-
+folkbokförinsAdress.innerHTML += "Folkbokförings Adress: " + obj.attribut.folkbokförinsAdress 
+särskildpostAdress.innerHTML += `Särskild post-Adress: ${obj.attribut.särskildpostAdress}`
+EpostadressaArbetet.innerHTML += `Epostadress Arbetet: ${obj.attribut.EpostadressaArbetet}`
+län.innerHTML += "Län: " + obj.attribut.län
+kommun.innerHTML += "Kommun: " + obj.attribut.kommun
+kod.innerHTML += "Kod: " + obj.attribut.kod
+TypAvMedarbetare.innerHTML += "Typ Av Medarbetare: " + obj.attribut.TypAvMedarbetare
   
-    data.append(tilltalsnamn,efternamn,fdatum)
+data.append(tilltalsnamn,efternamn,fdatum,folkbokförinsAdress,särskildpostAdress,EpostadressaArbetet,län,kommun,kod,TypAvMedarbetare)
     
   
 }).catch(function(err){
