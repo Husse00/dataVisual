ea84c7cadb3ca85f212a8be734f4b35ac7d46367
Husse00 <fhusal19@gmail.com>
2022-11-09 11:21:55 +0100

    updated layout

--- datavisual.js
+++ datavisual.js
@@ -1,10 +1,8 @@
 let root = document.getElementById("root")
 let data = document.getElementById("data")
 let divAttribute = document.getElementById("divAttribute")
-// CREATING PARAGRAPHS TO STORE THE FECTHED DATA IN.
-
-let personnummer = 
-
+let dataInfo = document.getElementById("dataDisplayId")
+let infoBtn = document.getElementById("infoBtn") 
 
 data.style.display = "none"
 divAttribute.addEventListener('click', ()=>{
@@ -12,5 +10,20 @@ divAttribute.addEventListener('click', ()=>{
         data.style.display = 'block';
     } else {
         data.style.display = 'none'
+        dataInfo.style.display = "none"
     }
+})
+
+dataInfo.style.display = "none"
+infoBtn.addEventListener('click', ()=>{
+
+if(dataInfo.style.display == 'none')
+{
+    dataInfo.style.display = "block"
+    infoBtn.style.backgroundColor = "#b41083"
+} else {
+    dataInfo.style.display = "none"
+    infoBtn.style.backgroundColor = "#3c0580"
+}
+
 })
